50d61717e4bb98d50ec7a1373858befaa6f8615b
Allan Assis <allan-bdc@hotmail.com>
2018-10-02 23:50:34 -0300

    Add ThumbPreview component

--- src/components/ThumbPreview.js
+++ src/components/ThumbPreview.js
@@ -1,5 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
+import Dotdotdot from "react-dotdotdot";
+import { Card, CardContent, Typography } from "@material-ui/core";
 
-const ThumbPreview = props => <div>Hello World</div>;
+const ThumbPreview = props => (
+  <div style={{ ...props.style, margin: 16 }}>
+    <Card>
+      <CardContent>
+        <Typography variant="headline" component="h2">
+          <Dotdotdot clamp={4}>{props.text}</Dotdotdot>
+        </Typography>
+      </CardContent>
+    </Card>
+  </div>
+);
 
 export default ThumbPreview;
