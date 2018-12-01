6dd8214e34d291c24d8e9a8ee29b0b300c3b3e23
Allan Assis <allan-bdc@hotmail.com>
2018-12-01 18:01:56 -0200

    Add configuration to Config.js

---
+++ src/Config.js
@@ -0,0 +1,16 @@
+export const URL_SERVICE = "https://flask-service-bd2.herokuapp.com"
+export const headers = { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' }
+export const initialState = {
+    "blocks": [
+        {
+            "key": "4r5r7",
+            "text": "Digite aqui o seu text",
+            "type": "unstyled",
+            "depth": 0,
+            "inlineStyleRanges": [],
+            "entityRanges": [],
+            "data": {}
+        }
+    ],
+    "entityMap": {}
+}
