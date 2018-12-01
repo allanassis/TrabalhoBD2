6eea5582a85334a5009cf7d3906bd53cc392709e
Allan Assis <allan-bdc@hotmail.com>
2018-12-01 18:28:16 -0200

    Updates

---
+++ precache-manifest.a4dcf985338bc5d5bd0a8dfcf97c1937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3158426e9eb3d72d979",
+    "url": "/trabalho-bd2/static/js/runtime~main.e3158426.js"
+  },
+  {
+    "revision": "6708323472572d67fd59",
+    "url": "/trabalho-bd2/static/js/main.67083234.chunk.js"
+  },
+  {
+    "revision": "5ddf2473e47b2890ecec",
+    "url": "/trabalho-bd2/static/js/1.5ddf2473.chunk.js"
+  },
+  {
+    "revision": "6708323472572d67fd59",
+    "url": "/trabalho-bd2/static/css/main.59b18268.chunk.css"
+  },
+  {
+    "revision": "5ddf2473e47b2890ecec",
+    "url": "/trabalho-bd2/static/css/1.da0045d6.chunk.css"
+  },
+  {
+    "revision": "39717f8dec7d1fdbcc7659e7e2f98844",
+    "url": "/trabalho-bd2/index.html"
+  }
+];
